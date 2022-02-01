7a4a7c3d40845d725d99275567a0e0cae77d2642
Dmitr <Dmitr@DESKTOP-H2P4N61>
2022-02-01 18:00:58 +0200

    Добавьте файлы проекта.

---
+++ BaseLab11.1/Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLab11._1
+{
+    public class Class
+    {
+        public int First { get; set; } = 0;
+        public int Second { get; set; } = 0;
+
+        public double Result()
+        {
+            return First * Second * 1 / 2;
+        }
+
+        public string Print()
+        {
+            return $"Площадь прямоугольного треуголинка равна : {Result():F2}";
+        }
+    }
+}
